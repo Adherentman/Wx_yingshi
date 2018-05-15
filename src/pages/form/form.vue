4d65e101655d40743139c917593cf71e46a643bc
macxu <adherentxu@gmail.com>
2018-05-15 22:33:06 +0800

    认识vue

---
+++ src/pages/form/form.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <p>hello world!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
