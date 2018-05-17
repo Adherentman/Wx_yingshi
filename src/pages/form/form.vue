735ce94afbbdbe8503973e934f21add58fd868c8
macxu <adherentxu@gmail.com>
2018-05-17 11:27:09 +0800

    Liao jie from

--- src/pages/form/form.vue
+++ src/pages/form/form.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="container">
-    <p>hello world!</p>
-  </div>
+  <form @submit="formSubmit" report-submit>
+    <view>
+      <view>input:</view>
+      <input name="input" placeholder="please input here" focus="true"/>
+    </view>
+      <button form-type="submit">Submit</button>
+  </form>
 </template>
 
 <script>
@@ -9,7 +13,11 @@ export default {
   data () {
   },
   methods: {
+    formSubmit: function(e) {
+    console.log('form发生了submit事件，携带数据为：', e.mp.detail.value)
   }
+  },
+  
 }
 </script>
 
