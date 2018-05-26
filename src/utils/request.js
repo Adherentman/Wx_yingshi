ce47939c4c8a2d278dbc97ce8f6497370485fc81
macxu <adherentxu@gmail.com>
2018-05-26 17:58:29 +0800

    Format request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,11 +2,11 @@ const maoyanApi = 'http://m.maoyan.com/';
 
 export const MaoYanRequest = (options, data = {}, method) => new Promise((resolve, rejet) => {
   const url = maoyanApi + options;
-  wx.request({
-    url,
-    data,
-    method,
-    success: res => resolve(res.data),
-    fail: err => rejet(err)
-  })
+    wx.request({
+      url,
+      data,
+      method,
+      success: res => resolve(res.data),
+      fail: err => rejet(err)
+    })
 }) 
